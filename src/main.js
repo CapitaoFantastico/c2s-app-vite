4161540349513e831a2023559bf02d62d1b569e8
CapitaoFantastico <henriquesouza182@gmail.com>
2022-02-08 17:24:11 -0300

    Configuração Pinia

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 
 import router from "./router";
+import { createPinia } from "pinia";
 
 loadFonts();
 
-createApp(App).use(router).use(vuetify).mount("#app");
+createApp(App).use(router).use(createPinia()).use(vuetify).mount("#app");
---
+++ src/store/main.js
@@ -0,0 +1,9 @@
+import { defineStore } from "pinia";
+
+export const main = defineStore("main", {
+  state: () => ({
+    count: 0,
+  }),
+  getters: {},
+  actions: {},
+});
