00c9eba107366c65f850e3788a009a0947518cb1
CapitaoFantastico <henriquesouza182@gmail.com>
2022-02-08 12:28:39 -0300

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-alert type="success">{{ counter }}</v-alert>
+  </v-container>
+</template>
+
+<script setup>
+import { onMounted, ref, computed } from "vue";
+
+let counter = ref(0);
+
+setInterval(() => {
+  counter.value++;
+  console.log(counter.value);
+}, 5000);
+</script>
